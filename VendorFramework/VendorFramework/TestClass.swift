552187bf516bb7cfe0b746fc6979fa1168b6da49
Rahul Singh <rahulsingh@nocs-MacBook-Pro.local>
2021-07-29 19:51:59 +0530

    Initial commit

---
+++ VendorFramework/VendorFramework/TestClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestClass.swift
+//  VendorFramework
+//
+//  Created by Rahul Singh on 29/07/21.
+//
+
+import Foundation
+
+public class TestClass {
+    public static func testFunc() -> String {
+        "Test success"
+    }
+}
---
+++ VendorFramework/VendorFramework/VendorFramework.h
@@ -0,0 +1,18 @@
+//
+//  VendorFramework.h
+//  VendorFramework
+//
+//  Created by Rahul Singh on 29/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VendorFramework.
+FOUNDATION_EXPORT double VendorFrameworkVersionNumber;
+
+//! Project version string for VendorFramework.
+FOUNDATION_EXPORT const unsigned char VendorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VendorFramework/PublicHeader.h>
+
+
